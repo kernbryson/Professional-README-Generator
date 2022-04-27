e72345efe2f37cdf478fa35b5a52a0478603ae01
Bryson Kern <kernbryson@yahoo.com>
2022-04-27 14:37:36 -0400

    added questions array and set up inquirer

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require("fs");
+// const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+
+    name: "projectTitle",
+
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+
+    name: "name",
+
+    message: "What is your name?",
+  },
+  {
+    type: "input",
+
+    name: "projectDescription",
+
+    message: "Enter a description of your project.",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((data) => {
+    console.log(data);
+    // const createReadMe = generateMarkdown(answers);
+  });
+}
 
 // Function call to initialize app
 init();
