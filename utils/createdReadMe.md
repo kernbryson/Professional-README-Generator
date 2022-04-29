30afbbdfd7d7d5a84ee1308668b99ce24cb6c8e2
Bryson Kern <kernbryson@yahoo.com>
2022-04-29 14:42:15 -0400

    started creating badges

---
+++ utils/createdReadMe.md
@@ -0,0 +1,37 @@
+
+  # d-d
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+## Description 
+d
+
+
+## Table of Contents 🧙
+
+- [installation](#installation)
+- [Usage](#usage)
+- [Tests](#Tests)
+- [Credits](#credits)
+- [License](#license)
+- [Questions](#Questions)
+
+## Installation
+d
+
+## Usage
+
+d
+
+## Tests
+d
+
+## Credits
+
+d
+
+## License
+This project uses the MIT license.
+
+## Questions
+Feel free to contact me with any questions or concerns!
+ - email: d
+ - GitHub: https://github.com/d
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,30 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let badge = "";
+  switch (license) {
+    case "MIT":
+      badge =
+        "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+      break;
+    case "GNU":
+      badge =
+        "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+      break;
+    case "Mozilla":
+      badge =
+        "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+      break;
+    case "Unlicense":
+      badge =
+        "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)";
+      break;
+    case "None":
+      badge = "";
+      break;
+  }
+  return badge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,19 +36,22 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  let badge = renderLicenseBadge(data.license);
   return `
   # ${data.projectTitle}-${data.Name}
-
-## Description
+${badge}
+## Description 
 ${data.projectDescription}
 
 
-## Table of Contents
+## Table of Contents 🧙
 
 - [installation](#installation)
 - [Usage](#usage)
+- [Tests](#Tests)
 - [Credits](#credits)
 - [License](#license)
+- [Questions](#Questions)
 
 ## Installation
 ${data.installation}
@@ -33,7 +60,7 @@ ${data.installation}
 
 ${data.usage}
 
-## Tests Performed
+## Tests
 ${data.tests}
 
 ## Credits
@@ -44,9 +71,9 @@ ${data.credits}
 This project uses the ${data.license} license.
 
 ## Questions
-Feel free to contact me with any questions or concerns
+Feel free to contact me with any questions or concerns!
  - email: ${data.email}
- - GitHub: github.com/${data.github}
+ - GitHub: https://github.com/${data.github}
 `;
 }
 
